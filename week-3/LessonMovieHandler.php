82ce8e74a03d7f03d15bfbc386623fd5165ccfd4
Aiden <132271900+348615336@users.noreply.github.com>
2026-05-21 18:19:30 -0400

    fixed depressing code

---
+++ week-3/LessonMovieHandler.php
@@ -0,0 +1,26 @@
+<?php
+    class LessonMovieHandler{
+        private $targetURL;
+        private $securityKey;
+
+        public function __construct($incomingURL, $incomingKey){
+            $this->targetURL = $incomingURL;
+            $this->securityKey = $incomingKey;
+        }
+
+        // this pulls the movie dataset from the API
+
+        public function fetchPopular($selectedPage = 1){
+            // constructing the string with newly assigned class properties
+            $endpointUrl = "{$this->targetURL}/movie/popular?api_key={$this->securityKey}&language=en-US&page=" . intval($selectedPage);
+            $rawJsonString = @file_get_contents($endpointUrl);
+            if ($rawJsonString === false){
+                return [];
+            }
+            $decodedPayLoad = json_decode($rawJsonString);
+            return $decodedPayLoad->results ?? [];
+        }
+
+    }
+
+?>
---
+++ week-3/config.php
@@ -0,0 +1,8 @@
+<?php
+    define("TMDB_API_KEY", "5790225afc0a638a6ed55fee622c3bd4");
+
+    //now add a base url for TMDB
+
+    define("TMDB_API_URL", "https://api.themoviedb.org/3")
+
+?>
